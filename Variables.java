197dcb930e9be8a68bccbebbf190e59b474f464e
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-09-20 00:10:50 +0600

    Add variable declaration

---
+++ Variables.java
@@ -0,0 +1,25 @@
+class Variables {
+
+    // String - stores text
+    // int - stores integer
+    // float - stores decimal
+    // char - stores character
+    // boolean - true or false
+
+    public static void main(String args[]) {
+
+        String myName = "Wasik Ahmed";
+        int age = 22;
+        float cgpa = 12.5f;
+        char sex = 'M';
+        boolean isTall = true;
+
+        // print variables
+        System.out.println("My name is " + myName);
+        System.out.println("My age: " + age);
+        System.out.println("My cgpa: " + cgpa);
+        System.out.println("Sex: " + sex);
+        System.out.println("Tall: " + isTall);
+
+    }
+}
