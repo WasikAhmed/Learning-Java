a1ef1b3e2f994e2adaeb331a55b34f108a8dab6c
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-09-24 13:43:41 +0600

    Update Input.java Add input illustration of different data types

--- Input.java
+++ Input.java
@@ -1,19 +1,42 @@
+// Use Scanner class for input
+// Scanner class is in java.util package
+
 import java.util.*;
 
+// methods
+// nextInt() -> input integer
+// nextLine() -> input string
+// nextFloat() -> input float
+// nextDouble() -> input double
+// nextByte() -> input byte
+// nextBoolean() -> input boolean
+// nextLong() -> input long
+// nextShort -> input short
 
 public class Input {
     
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        // creating an object of Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        String Name;
+        int Age;
+        float Cgpa;
 
         System.out.print("Please enter your name: ");
+        Name = sc.nextLine();
 
-        String Name = input.nextLine();
+        System.out.print("Please enter you age: ");
+        Age = sc.nextInt();
 
-        String WelcomeMessage = "Hello " + Name;
+        System.out.print("Please enter your CGPA: ");
+        Cgpa = sc.nextFloat();
 
-        System.out.println(WelcomeMessage);
+        System.out.println("Your details: ");
+        System.out.println("Name: " + Name);
+        System.out.println("Age: " + Age);
+        System.out.println("Cgpa: " + Cgpa);
 
     }
 
